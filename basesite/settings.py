7b50172b957d1e85f76f4543cbfc6e9f63ae9a29
autosaver <adityasingh1235467890@gmail.com>
2020-07-13 10:07:40 +0530

    First commit

--- basesite/settings.py
+++ basesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'basesitepage',
     'weather',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,9 +56,7 @@ ROOT_URLCONF = 'basesite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-                os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
