82876c06daf0f920d25b2da2072f56c761e00f31
autosaver <adityasingh1235467890@gmail.com>
2020-07-13 09:59:51 +0530

    base page

--- basesite/urls.py
+++ basesite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('weather/', include('weather.urls'))
+    path('weather/', include('weather.urls')),
+    path('',include('basesitepage.urls')),
 ]
